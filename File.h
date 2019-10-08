f0ce35554d7dbf1a267b1a0e0fbbfbb1a484b329
MarinaMontag <43826689+MarinaMontag@users.noreply.github.com>
2019-10-08 12:47:44 +0300

    Update File.h

--- File.h
+++ File.h
@@ -13,6 +13,7 @@ public:
 	File(path full_path);
 	void Set_name(std::string name);
 	void Set_path(path parent_path);
+	void Set_modification(time_t modification);
 	path Get_fullpath();
 	std::string Get_name();
 	std::string Get_mode();
